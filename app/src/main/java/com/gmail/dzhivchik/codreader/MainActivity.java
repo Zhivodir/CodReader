96533108498a98f774f498eebac7b6e6b6bbd49e
zhivodir <dzhivchik@gmail.com>
2018-02-14 15:53:42 +0200

    some smaller code

--- app/src/main/java/com/gmail/dzhivchik/codreader/MainActivity.java
+++ app/src/main/java/com/gmail/dzhivchik/codreader/MainActivity.java
@@ -1,20 +1,18 @@
 package com.gmail.dzhivchik.codreader;
 
+import android.app.Activity;
 import android.content.pm.ActivityInfo;
-import android.graphics.Color;
-import android.graphics.Matrix;
-import android.graphics.RectF;
 import android.hardware.Camera;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import android.hardware.Camera.AutoFocusCallback;
+import android.hardware.Camera.PreviewCallback;
+import android.hardware.Camera.Size;
 import android.os.Bundle;
-import android.view.Display;
-import android.view.Surface;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.Window;
-import android.view.WindowManager;
+import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import net.sourceforge.zbar.Config;
@@ -23,194 +21,128 @@ import net.sourceforge.zbar.ImageScanner;
 import net.sourceforge.zbar.Symbol;
 import net.sourceforge.zbar.SymbolSet;
 
-import java.io.IOException;
-
-public class MainActivity extends AppCompatActivity {
-
-    private ImageScanner scanner;
-    private SurfaceView sv;
-    private SurfaceHolder holder;
-    private HolderCallback holderCallback;
-    private Camera camera;
-    private Camera.PreviewCallback previewCb;
+public class MainActivity extends Activity {
+    private static final int WHITE = 0xFFFFFFFF;
+    private static final int BLACK = 0xFF000000;
+    private Camera mCamera;
+    private CameraPreview mPreview;
     private Handler autoFocusHandler;
     private FrameLayout preview;
+    private TextView scanText;
+    private ImageView bar_code;
+    private EditText code_for_bar;
+    private ImageScanner scanner;
+    private boolean barcodeScanned = false;
+    private boolean previewing = true;
+    private String lastScannedCode;
     private Image codeImage;
-
-    final int CAMERA_ID = 0;
-    final boolean FULL_SCREEN = true;
 
     static {
         System.loadLibrary("iconv");
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
 
-        sv = (SurfaceView) findViewById(R.id.surfaceView);
-        holder = sv.getHolder();
-        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-
-        holderCallback = new HolderCallback();
-        holder.addCallback(holderCallback);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-//        autoFocusHandler = new Handler();
 
+
+        autoFocusHandler = new Handler();
+
+        preview = (FrameLayout) findViewById(R.id.cameraPreview);
+
+
+        /* Instance barcode scanner */
         scanner = new ImageScanner();
-        scanner.setConfig(0, Config.X_DENSITY, 3); //почему именно эти параметры нигде не указано
+        scanner.setConfig(0, Config.X_DENSITY, 3);
         scanner.setConfig(0, Config.Y_DENSITY, 3);
 
-        Camera.PreviewCallback previewCb = new Camera.PreviewCallback() {
-            public void onPreviewFrame(byte[] data, Camera camera) {
-                String lastScannedCode = null;
-                codeImage.setData(data);
-                int result = scanner.scanImage(codeImage);
-                System.out.println(result);
-                if (result != 0) {
-                    SymbolSet syms = scanner.getResults();
-                    for (Symbol sym : syms) {
-                        lastScannedCode = sym.getData();
-                    }
-                }
-                TextView info = (TextView) findViewById(R.id.result);
-                info.setTextColor(Color.WHITE);
-                info.setText(lastScannedCode);
-            }
-        };
+        scanText = (TextView) findViewById(R.id.scanText);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        camera = Camera.open(CAMERA_ID);
-        setPreviewSize(FULL_SCREEN);
+        resumeCamera();
     }
 
-    @Override
-    protected void onPause() {
+    public void onPause() {
         super.onPause();
-        if (camera != null)
-            camera.release();
-        camera = null;
+        releaseCamera();
     }
 
-    class HolderCallback implements SurfaceHolder.Callback {
+    /**
+     * A safe way to get an instance of the Camera object.
+     */
+    public static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open();
+        } catch (Exception e) {
+            //
+        }
+        return c;
+    }
 
-        @Override
-        public void surfaceCreated(SurfaceHolder holder) {
-            try {
-                camera.setPreviewDisplay(holder);
-                camera.startPreview();
-            } catch (IOException e) {
-                e.printStackTrace();
+    private void releaseCamera() {
+        if (mCamera != null) {
+            previewing = false;
+            mCamera.cancelAutoFocus();
+            mCamera.setPreviewCallback(null);
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    private void resumeCamera() {
+        scanText.setText(getString(R.string.scan_process_label));
+        mCamera = getCameraInstance();
+        mPreview = new CameraPreview(this, mCamera, previewCb, autoFocusCB);
+        preview.removeAllViews();
+        preview.addView(mPreview);
+        if (mCamera != null) {
+            Camera.Parameters parameters = mCamera.getParameters();
+            Size size = parameters.getPreviewSize();
+            codeImage = new Image(size.width, size.height, "Y800");
+            previewing = true;
+            mPreview.refreshDrawableState();
+        }
+    }
+
+    private Runnable doAutoFocus = new Runnable() {
+        public void run() {
+            if (previewing && mCamera != null) {
+                mCamera.autoFocus(autoFocusCB);
             }
         }
+    };
 
-        @Override
-        public void surfaceChanged(SurfaceHolder holder, int format, int width,
-                                   int height) {
-            camera.stopPreview();
-            setCameraDisplayOrientation(CAMERA_ID);
-            try {
-                camera.setPreviewDisplay(holder);
-                camera.startPreview();
-            } catch (Exception e) {
-                e.printStackTrace();
+    PreviewCallback previewCb = new PreviewCallback() {
+        public void onPreviewFrame(byte[] data, Camera camera) {
+//            Log.d("CameraTestActivity", "onPreviewFrame data length = " + (data != null ? data.length : 0));
+            codeImage.setData(data);
+            int result = scanner.scanImage(codeImage);
+            if (result != 0) {
+                SymbolSet syms = scanner.getResults();
+                for (Symbol sym : syms) {
+                    lastScannedCode = sym.getData();
+                    if (lastScannedCode != null) {
+                        scanText.setText(getString(R.string.scan_result_label) + lastScannedCode);
+                        barcodeScanned = true;
+                    }
+                }
             }
+            camera.addCallbackBuffer(data);
         }
+    };
 
-        @Override
-        public void surfaceDestroyed(SurfaceHolder holder) {
-
+    // Mimic continuous auto-focusing
+    final AutoFocusCallback autoFocusCB = new AutoFocusCallback() {
+        public void onAutoFocus(boolean success, Camera camera) {
+            autoFocusHandler.postDelayed(doAutoFocus, 1000);
         }
-
-    }
-
-    void setPreviewSize(boolean fullScreen) {
-
-        // получаем размеры экрана
-        Display display = getWindowManager().getDefaultDisplay();
-        boolean widthIsMax = display.getWidth() > display.getHeight();
-
-        // определяем размеры превью камеры
-        Camera.Size size = camera.getParameters().getPreviewSize();
-
-        RectF rectDisplay = new RectF();
-        RectF rectPreview = new RectF();
-
-        // RectF экрана, соотвествует размерам экрана
-        rectDisplay.set(0, 0, display.getWidth(), display.getHeight());
-
-        // RectF первью
-        if (widthIsMax) {
-            // превью в горизонтальной ориентации
-            rectPreview.set(0, 0, size.width, size.height);
-        } else {
-            // превью в вертикальной ориентации
-            rectPreview.set(0, 0, size.height, size.width);
-        }
-
-        Matrix matrix = new Matrix();
-        // подготовка матрицы преобразования
-        if (!fullScreen) {
-            // если превью будет "втиснут" в экран (второй вариант из урока)
-            matrix.setRectToRect(rectPreview, rectDisplay,
-                    Matrix.ScaleToFit.START);
-        } else {
-            // если экран будет "втиснут" в превью (третий вариант из урока)
-            matrix.setRectToRect(rectDisplay, rectPreview,
-                    Matrix.ScaleToFit.START);
-            matrix.invert(matrix);
-        }
-        // преобразование
-        matrix.mapRect(rectPreview);
-
-        // установка размеров surface из получившегося преобразования
-        sv.getLayoutParams().height = (int) (rectPreview.bottom);
-        sv.getLayoutParams().width = (int) (rectPreview.right);
-    }
-
-    void setCameraDisplayOrientation(int cameraId) {
-        // определяем насколько повернут экран от нормального положения
-        int rotation = getWindowManager().getDefaultDisplay().getRotation();
-        int degrees = 0;
-        switch (rotation) {
-            case Surface.ROTATION_0:
-                degrees = 0;
-                break;
-            case Surface.ROTATION_90:
-                degrees = 90;
-                break;
-            case Surface.ROTATION_180:
-                degrees = 180;
-                break;
-            case Surface.ROTATION_270:
-                degrees = 270;
-                break;
-        }
-
-        int result = 0;
-
-        // получаем инфо по камере cameraId
-        Camera.CameraInfo info = new Camera.CameraInfo();
-        Camera.getCameraInfo(cameraId, info);
-
-        // задняя камера
-        if (info.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
-            result = ((360 - degrees) + info.orientation);
-        } else
-            // передняя камера
-            if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
-                result = ((360 - degrees) - info.orientation);
-                result += 360;
-            }
-        result = result % 360;
-        camera.setDisplayOrientation(result);
-    }
-
+    };
 }
